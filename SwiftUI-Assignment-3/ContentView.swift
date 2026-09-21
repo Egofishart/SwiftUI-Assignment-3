12be77a7d79c6915aa608a58a3c2755a87a9a968
Egofishart <nikodanelia16@gmail.com>
2026-09-21 14:27:35 +0400

    Initial commit

---
+++ SwiftUI-Assignment-3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Assignment-3
+//
+//  Created by NikoDanelia on 21/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Assignment-3/SwiftUI_Assignment_3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Assignment_3App.swift
+//  SwiftUI-Assignment-3
+//
+//  Created by NikoDanelia on 21/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Assignment_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
